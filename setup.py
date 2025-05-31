e8f4fabedcfeb27dd0e56c54a803fcfc03209e5e
Gökhan <ghb@enzianlabs.com>
2025-05-31 18:55:19 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -12,18 +12,9 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.12",
-    packages=find_packages(exclude=["tests"]),
     install_requires=[
         "livekit>=1.0.8,<2.0.0",
-        "pytest>=7.4.4",
     ],
-    extras_require={
-        "test": [
-            "pytest>=7.0.0,<8.0.0",
-            "pytest-asyncio>=0.21.0,<0.22.0",
-            "pytest-cov>=4.1.0,<5.0.0",
-        ],
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.12",
